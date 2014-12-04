3f634e8097ca50b54833e98f9139f63030142f22
gabe <GabeMaker@users.noreply.github.com>
2014-12-04 14:17:12 +0000

    added user input for student names

--- directory.rb
+++ directory.rb
@@ -1,13 +1,3 @@
-# students into array
-students = [
-	{:name => "Bibiana Am", :cohort => :dec},
-	{:name => "Jin Da", :cohort => :dec},
-	{:name => "Sanda Go", :chohort => :dec},
-	{:name => "Hannah Ca", :cohort => :dec},
-	{:name => "Huy Le", :cohort => :dec},
-	{:name => "Marcin Wa", :cohort => :dec}
-]
-
 def print_header
 	puts "The students of my chohort at Makers Academy"
 	puts "-------------"
@@ -19,10 +9,41 @@ def print(students)
 	end
 end
 
+def input_students
+	puts "Please entere the names of the students"
+	puts "To finish, just hit return twice"
+	#empty array
+	students = []
+	#get first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add student hash to array
+		students << {:name => name, :cohort => :december}
+		puts "Now we have #{students.length} students"
+		# get another name
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end 
+
 def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+# # students into array
+# students = [
+# 	{:name => "Bibiana Am", :cohort => :dec},
+# 	{:name => "Jin Da", :cohort => :dec},
+# 	{:name => "Sanda Go", :chohort => :dec},
+# 	{:name => "Hannah Ca", :cohort => :dec},
+# 	{:name => "Huy Le", :cohort => :dec},
+# 	{:name => "Marcin Wa", :cohort => :dec}
+# ]
+
