c0375e5cdc07926999496c8488fbc6b07bc25775
gabe <GabeMaker@users.noreply.github.com>
2014-12-04 11:27:24 +0000

    Added student count

--- directory.rb
+++ directory.rb
@@ -7,3 +7,7 @@ puts "Sanda Go..."
 puts "Hannah Car" 
 puts "Huy Le..."
 puts "Marcin Wa..."
+print "Overall, we have "
+print 9
+puts " great students"
+# print "line1\nline2\nline3\n" - testing manual line breaks
